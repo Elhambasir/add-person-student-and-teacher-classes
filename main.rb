d2b53ca1fb09c00797ec134d83df6575024337b0
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-06-27 12:06:01 +0430

    Update main.rb
    Fix linter error

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
-require './person.rb'
-require './trimmerDecorator.rb'
-require './capitalizeDecorator.rb'
+require './person'
+require './trimmer_decorator'
+require './capitalize_decorator'
 
-person = Person.new('maximilianus',22)
+person = Person.new('maximilianus', 22)
 person.correct_name
 capitalized_person = CapitalizeDecorator.new(person)
 puts capitalized_person.correct_name
