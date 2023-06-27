1cd7c136d4966941102b58daa80835b09f4be750
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-06-27 11:20:09 +0430

    Update person.rb
    1. Modify class person to inherit from class nameable

--- person.rb
+++ person.rb
@@ -1,9 +1,14 @@
-class Person
+require './nameable.rb'
+
+class Person < Nameable
   def initialize(name = 'Unknown', age = 0, parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+  def correct_name 
+    @name
   end
 
   attr_accessor :name, :age
