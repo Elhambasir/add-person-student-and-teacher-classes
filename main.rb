e50352ca5ed0ec41fcae866bc0528e76132f8d0f
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-03 19:18:37 +0430

    Update main.rb
    Implement SOLID principles

--- main.rb
+++ main.rb
@@ -1,15 +1,5 @@
 require_relative 'app'
-
-def display
-  puts 'Please choose an option by entering a number:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
-end
+require_relative 'menu'
 
 def run_app(app)
   menu_options = {
@@ -19,25 +9,31 @@ def run_app(app)
     '4' => :create_book,
     '5' => :create_rental,
     '6' => :list_rentals,
-    '7' => :exit
+    '7' => :exit_app
   }
+  menu = Menu.new
   loop do
-    display
-    input = gets.chomp
-
+    menu.display_menu
+    input = menu.user_input
     if menu_options.key?(input)
-      run = menu_options[input]
-      app.send(run)
-      break if run == 'exit'
+      app.send(menu_options[input])
+      break if menu_options[input] == :exit_app.to_s
     else
-      puts 'Enter the correct option: '
+      puts 'Invalid option. Please try again.'
     end
   end
 end
 
+def handle_menu_input(app, input)
+  run = menu_options[input]
+  app.send(run)
+end
+
 def main
   app = App.new
+
   puts 'Welcome to the School Library App!'
+
   run_app(app)
 end
 
