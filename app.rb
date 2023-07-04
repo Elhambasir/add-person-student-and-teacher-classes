d9fdd2d54bbc0d3f51e59d9f68e55d329cc923a5
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-04 16:20:48 +0430

    Update app.rb
    Use input validator and requester

--- app.rb
+++ app.rb
@@ -5,7 +5,10 @@ require './book'
 require './rental'
 require './student'
 require './teacher'
+require './input_validator'
+require './input_requester'
 require 'pry'
+
 class App
   def initialize
     @people = []
@@ -21,20 +24,17 @@ class App
   end
 
   def create_person
-    print 'Do you want to create a student (1) or a teacher (2)? '
-    person_type = gets.chomp.to_i
-    print 'Name:'
-    name = gets.chomp
-    print 'Age:'
-    age = gets.chomp.to_i
+    input_requester = InputRequester.new
+    input_validator = InputValidator.new
+    person_type = input_validator.validate_person_type(input_requester.ask_for_person_type)
+    name = input_validator.validate_person_name(input_requester.ask_for_person_name)
+    age = input_validator.validate_person_age(input_requester.ask_for_person_age)
     if person_type == 1
-      print 'Whats the parent permission? (y/n) '
-      parent_permission = gets.chomp == 'y'
+      parent_permission = input_validator.validate_parent_permission(input_requester.ask_for_parent_permission)
       @people << Student.new(name, age, parent_permission)
       puts 'Student created!'
     elsif person_type == 2
-      print 'Specialization:'
-      specialization = gets.chomp
+      specialization = input_validator.validate_specialization(input_requester.ask_for_teacher_specialization)
       @people << Teacher.new(name, age, specialization)
       puts 'Teacher created!'
     else
@@ -43,33 +43,33 @@ class App
   end
 
   def create_book
-    print 'Title:'
-    title = gets.chomp
-    print 'Author:'
-    author = gets.chomp
+    input_requester = InputRequester.new
+    input_validator = InputValidator.new
+    title = input_validator.validate_book_title(input_requester.ask_for_book_title)
+    author = input_validator.validate_book_author(input_requester.ask_for_book_author)
     @books << Book.new(title, author)
     puts 'Book created!'
   end
 
   def create_rental
-    puts 'Choose a book from the list by number'
+    input_requester = InputRequester.new
+    input_validator = InputValidator.new
     @books.each_with_index { |book, index| puts "#{index} - #{book.title} by #{book.author}" }
-    book_index = gets.chomp.to_i
+    book_index = input_requester.ask_for_rental_book_index
     book = @books[book_index]
-    puts 'Choose a person from the list by number:'
     @people.each_with_index { |person, index| puts "#{index} - #{person.name} - #{person.age} - #{person.class}" }
-    person_index = gets.chomp.to_i
+    person_index = input_requester.ask_for_rental_person_index
     person = @people[person_index]
-    puts 'Date in format yyyy/mm/dd:'
-    date = gets.chomp
+    date = input_validator.validate_rental_date(input_requester.ask_for_rental_date)
     Rental.new(date, book, person)
     puts 'Rental created!'
   end
 
   def list_rentals
+    input_requester = InputRequester.new
+    InputValidator.new
     @people.each { |person| puts "ID: #{person.id}, Name: #{person.name}" }
-    print 'ID of person: '
-    person_id = gets.chomp.to_i
+    person_id = input_requester.ask_for_rental_person_id
     puts 'Rentals: '
     person_info = @people.find { |person| person.id == person_id }
     person_info.rentals.each { |rental| puts "Date: #{rental.date}, Book: #{rental.book.title}" }
