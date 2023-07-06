89ccca207778acfc208b7f93d58277e568b3111a
ActiveApps <douglas.luzinda.ot@gmail.com>
2023-07-06 14:43:44 +0300

    fix linters

--- app.rb
+++ app.rb
@@ -1,13 +1,12 @@
-require_relative 'storage'
-require_relative 'person'
-require_relative 'trimmer_decorator'
-require_relative 'capitalize_decorator'
-require_relative 'book'
-require_relative 'rental'
-require_relative 'student'
-require_relative 'teacher'
-require_relative 'input_validator'
-require_relative 'input_requester'
+require './person'
+require './trimmer_decorator'
+require './capitalize_decorator'
+require './book'
+require './rental'
+require './student'
+require './teacher'
+require './input_validator'
+require './input_requester'
 require 'pry'
 
 class App
@@ -23,10 +22,11 @@ class App
   end
 
   def list_people
-    @people = @storage_handler.load_data('people.json') || []
+    @people = @storage_handler.load_data('people.json')
+    return if @people.nil?
+
     @people.each do |person|
-      person_type = person[:person_type] == 1 ? 'Student' : 'Teacher'
-      puts "[#{person_type}] Name: #{person[:name]}, Age: #{person[:age]}"
+      puts "[#{person[:person_type] == 1 ? 'Student' : 'Teacher'}] Name: #{person[:name]}, Age: #{person[:age]}"
     end
   end
 
@@ -39,34 +39,69 @@ class App
     age = input_validator.validate_person_age(input_requester.ask_for_person_age)
 
     new_person = create_person_based_on_type(person_type, name, age, input_requester, input_validator)
-    @people = @storage_handler.load_data('people.json') || []
+    res = @storage_handler.load_data('people.json')
+    @people = check_if_res_is_array(res, @people)
     @people.push(new_person)
     @storage_handler.save_data('people.json', @people)
     puts 'Person created!'
   end
 
+  def check_if_res_is_array(res, people)
+    if !res.nil? && res.class != Array
+      people.push(res)
+    elsif !res.nil? && res.instance_of?(Array)
+      people = res
+    end
+    people
+  end
+
+  def create_student(person, person_type)
+    {
+      person_type: person_type,
+      name: person.name,
+      age: person.age,
+      parent_permission: person.parent_permission,
+      id: person.id
+    }
+  end
+
+  def create_teacher(person, person_type)
+    {
+      person_type: person_type,
+      name: person.name,
+      age: person.age,
+      specialization: person.specialization,
+      id: person.id
+    }
+  end
+
   def create_person_based_on_type(person_type, name, age, input_requester, input_validator)
     if person_type == 1
-      parent_permission = input_validator.validate_parent_permission(input_requester.ask_for_parent_permission)
-      Student.new(name, age, parent_permission).to_hash
+      person = Student.new(name, age,
+                           input_validator.validate_parent_permission(input_requester.ask_for_parent_permission))
+      create_student(person, person_type)
     elsif person_type == 2
-      specialization = input_validator.validate_specialization(input_requester.ask_for_teacher_specialization)
-      Teacher.new(name, age, specialization).to_hash
+      person = Teacher.new(name, age,
+                           input_validator.validate_specialization(input_requester.ask_for_teacher_specialization))
+      create_teacher(person, person_type)
     end
   end
 
   def create_book
     input_requester = InputRequester.new
     input_validator = InputValidator.new
-
     title = input_validator.validate_book_title(input_requester.ask_for_book_title)
     author = input_validator.validate_book_author(input_requester.ask_for_book_author)
-
     book = Book.new(title, author)
-    new_book = { title: title, author: author, id: book.id }
+    newbook = { title: title, author: author, id: book.id }
+    res = @storage_handler.load_data('books.json')
+    if !res.nil? && res.class != Array
+      @books.push(res)
+    elsif !res.nil? && res.instance_of?(Array)
+      @books = res
+    end
 
-    @books = @storage_handler.load_data('books.json') || []
-    @books.push(new_book)
+    @books.push(newbook)
     @storage_handler.save_data('books.json', @books)
     puts 'Book created!'
   end
@@ -75,8 +110,8 @@ class App
     input_requester = InputRequester.new
     input_validator = InputValidator.new
 
-    books = @storage_handler.load_data('books.json') || []
-    people = @storage_handler.load_data('people.json') || []
+    books = @storage_handler.load_data('books.json')
+    people = @storage_handler.load_data('people.json')
 
     puts 'Select a book from the list'
     books.each_with_index { |book, index| puts "#{index} - #{book[:title]} by #{book[:author]}" }
@@ -85,8 +120,7 @@ class App
 
     puts 'Select a person from the list'
     people.each_with_index do |person, index|
-      person_type = person[:person_type] == 1 ? 'Student' : 'Teacher'
-      puts "#{index} - #{person[:name]} - #{person[:age]} - #{person_type}"
+      puts "#{index} - #{person[:name]} - #{person[:age]} - #{person[:person_type] == 1 ? 'Student' : 'Teacher'}"
     end
     person_index = input_requester.ask_for_rental_person_index
     person = people[person_index]
@@ -94,10 +128,22 @@ class App
     date = input_validator.validate_rental_date(input_requester.ask_for_rental_date)
 
     rental = create_rental_object(date, book, person)
-    rentals = @storage_handler.load_data('rental.json') || []
-    rentals.push(rental)
-    @storage_handler.save_data('rental.json', rentals)
+    res = @storage_handler.load_data('rental.json')
+    res = check_if_res_is_empty(res, rental)
+    @storage_handler.save_data('rental.json', res)
     puts 'Rental created!'
+  end
+
+  def check_if_res_is_empty(res, rentals)
+    rental = []
+    if !res.nil? && res.class != Array
+      rental.push(res)
+      rental.push(rentals)
+    elsif !res.nil? && res.instance_of?(Array)
+      rental = res
+      rental.push(rentals)
+    end
+    rental
   end
 
   def create_rental_object(date, book, person)
@@ -110,18 +156,19 @@ class App
 
   def list_rentals
     input_requester = InputRequester.new
+    storage_handler = @storage_handler
 
-    people = @storage_handler.load_data('people.json') || []
-    books = @storage_handler.load_data('books.json') || []
-    rentals = @storage_handler.load_data('rental.json') || []
+    people = storage_handler.load_data('people.json')
+    books = storage_handler.load_data('books.json')
+    rentals = storage_handler.load_data('rental.json')
 
-    person_id = input_requester.ask_for_rental_person_id
-    person_rentals = rentals.select { |rental| rental[:person_id] == person_id }
+    pid = input_requester.ask_for_rental_person_id
+    rentals = rentals.select { |rental| rental[:person_id] == pid }
 
-    person_rentals.each do |rental|
+    rentals.each do |rental|
       book = books.find { |item| item[:id] == rental[:book_id] }
-      person = people.find { |person| person[:id] == person_id }
-      puts "#{rental[:date]} - #{book[:title]} - #{person[:name]}" unless book.nil? || person.nil?
+      persons = people.find { |person| person[:id] == pid }
+      puts "#{rental[:date]} - #{book[:title]} - #{persons[:name]}" unless book.nil? || persons.nil?
     end
   end
 
