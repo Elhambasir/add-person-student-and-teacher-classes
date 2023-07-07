1e471625638ff1a3b045588d19c7700226ba40b2
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-07 14:53:10 +0430

    Create book_spec.rb

---
+++ spec/book_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../book'
+
+# write test for class Book and its methods
+describe Book do
+  let(:book) { Book.new('test', 'elham') }
+
+  it 'Book must has a title' do
+    expect(book.title).to eq('test')
+  end
+
+  it 'Book must has an author' do
+    expect(book.author).to eq('elham')
+  end
+
+  it 'Book must has a rental' do
+    expect(book.rental).to eq([])
+  end
+
+  it 'Book must has an id' do
+    expect(book.id).to be_a(Integer)
+  end
+end
