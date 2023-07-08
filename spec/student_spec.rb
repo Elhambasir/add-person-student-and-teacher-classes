d8aa1f04488714f4d6e15c464d1bd1ee95e3df6a
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-08 07:40:53 +0430

    Update student_spec.rb
    Add test for play_hooky
    Add test for set the classroom of student

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,8 +1,10 @@
 require_relative '../student'
-
+require_relative '../classroom'
+require_relative '../person'
+require 'pry'
 describe Student do
   before :each do
-    @student = Student.new('John', 24, 'A')
+    @student = Student.new('John', 24, 'true')
   end
   it 'has a first name' do
     expect(@student.name).to eq('John')
@@ -10,10 +12,6 @@ describe Student do
 
   it 'has an age ' do
     expect(@student.age).to eq(24)
-  end
-
-  it 'has a parent permission but under age' do
-    expect(@student.parent_permission).to eq(true)
   end
 
   it 'has an id' do
@@ -24,11 +22,18 @@ describe Student do
     expect(@student.rental).to eq([])
   end
 
-  it 'has a classroom' do
-    expect(@student.classroom).to eq('A')
+  it 'should set the classroom of the student' do
+    student123 = Student.new('John', 24, parent_permission: true)
+    classroom = Classroom.new('A')
+    student123.classroom = classroom
+    expect(student123.classroom).to eq(classroom)
   end
 
   it 'can not use services (under age no parent permission)' do
     expect(@student.can_use_services?).to eq(true)
+  end
+
+  it 'play_hooky must return  \'¯\(ツ)/¯\' ' do
+    expect(@student.play_hooky).to eq('¯\(ツ)/¯')
   end
 end
