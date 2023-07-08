a0b3dcddcbf4fdf18f34fea815642f077412b13f
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-08 07:41:23 +0430

    Update classroom_spec.rb
    Add test for add_student method

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,16 +1,24 @@
 require_relative '../classroom'
-require_relative '../student'
-require_relative '../person'
-describe Classroom do
-  let(:classroom) { Classroom.new('CS101') }
-  let(:student1) { Student.new('Elham', 23, classroom) }
-  let(:student2) { Student.new('John', 24, classroom) }
 
-  it 'should have a label' do
-    expect(classroom.label).to eq('CS101')
+describe Classroom do
+  before :each do
+    @classroom = Classroom.new('CSD21')
   end
 
-  it 'should have an empty list of students' do
-    expect(classroom.students).to be_empty
+  it 'checking classroom instance' do
+    expect(@classroom).to be_instance_of Classroom
+  end
+
+  it 'checking attributes' do
+    expect(@classroom.label).to eq('CSD21')
+  end
+
+  it 'test for add student' do
+    student = double('student')
+    allow(student).to receive(:age) { '24' }
+    allow(student).to receive(:name) { 'elham' }
+    allow(student).to receive(:parent_permission) { true }
+    allow(student).to receive(:classroom=).and_return(Classroom)
+    expect(@classroom.add_student(student).students[0].name).to eq('elham')
   end
 end
