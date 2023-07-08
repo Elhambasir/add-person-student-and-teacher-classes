3a978b8a3bda9f3b138629cb59f399552e111d32
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-07-08 07:39:48 +0430

    Update student.rb

--- student.rb
+++ student.rb
@@ -1,16 +1,11 @@
 require_relative 'person'
 
 class Student < Person
-  attr_reader :classroom, :parent_permission
-
-  def initialize(name, age, classroom, parent_permission: true)
-    super(name, age, parent_permission)
-    @classroom = classroom
-  end
+  attr_reader :classroom
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.add_student(self) unless classroom.students.include?(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
